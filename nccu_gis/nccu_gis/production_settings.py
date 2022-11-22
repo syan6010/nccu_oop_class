c8fdd3b93172e29795199f9830409374a6adca0a
Syan6010 <45162124+syan6010@users.noreply.github.com>
2022-11-22 11:45:19 +0800

    Updata Heroku

--- nccu_gis/nccu_gis/production_settings.py
+++ nccu_gis/nccu_gis/production_settings.py
@@ -7,7 +7,7 @@ DATABASES = {
 }
 
 # Static asset configuration.
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
