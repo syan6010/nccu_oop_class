51378be97a0095a777d1cadcba338002eb76af92
Syan6010 <45162124+syan6010@users.noreply.github.com>
2022-11-22 12:04:30 +0800

    Update heroku

--- nccu_gis/nccu_gis/production_settings.py
+++ nccu_gis/nccu_gis/production_settings.py
@@ -7,13 +7,17 @@ DATABASES = {
 }
 
 # Static asset configuration.
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'staticfile'
+
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Allow all host headers.
 ALLOWED_HOSTS = ['*']
 
 # Turn off DEBUG mode.
-DEBUG = False
+DEBUG = True
