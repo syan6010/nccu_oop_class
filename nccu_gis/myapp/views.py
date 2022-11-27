f451069d6d1d77fe13f5d05ee547defbc3f3176a
Syan6010 <45162124+syan6010@users.noreply.github.com>
2022-11-28 01:22:23 +0800

    fix git conflict

--- nccu_gis/myapp/views.py
+++ nccu_gis/myapp/views.py
@@ -4,17 +4,13 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html", locals())
 
-
 def homepage_042(request):
     name = "108207442"
     return render(request, "homepage_042.html", locals())
 
-<<<<<<< HEAD
 def homepage_test(request):
     name = "test"
     return render(request, "homepage_test.html", locals())
-=======
 def homepage_000(request):
     name = "108207000"
     return render(request, "homepage_000.html", locals())
->>>>>>> 52332ce537354f5c2e239592dbf705593ac36fd8
